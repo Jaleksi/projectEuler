5c9e141bb07edf63af6d52b7e106e089a67b938f
Aleksi <aleksi.jok@gmail.com>
2019-06-05 18:32:38 +0300

    p25/

---
+++ p25/p25.py
@@ -0,0 +1,46 @@
+#
+# The Fibonacci sequence is defined by the recurrence relation:
+#
+#    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+#
+# Hence the first 12 terms will be:
+#
+#    F1 = 1
+#    F2 = 1
+#    F3 = 2
+#    F4 = 3
+#    F5 = 5
+#    F6 = 8
+#    F7 = 13
+#    F8 = 21
+#    F9 = 34
+#    F10 = 55
+#    F11 = 89
+#    F12 = 144
+#
+# The 12th term, F12, is the first term to contain three digits.
+#
+# What is the index of the first term in the Fibonacci sequence to contain
+# 1000 digits?
+#
+
+
+def fibo(n):
+    a = 1
+    b = 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+
+def main():
+    x = 1
+    while(True):
+        if(len(str(fibo(x))) > 1000):
+            print(x)
+            break
+        x += 1
+
+
+if(__name__ == "__main__"):
+    main()
