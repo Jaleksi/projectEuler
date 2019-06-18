07a33843214013764e325c296b5d2e133364a92a
Aleksi <aleksi.jok@gmail.com>
2019-06-18 17:10:58 +0300

    p52/

---
+++ problems/p52/p52.py
@@ -0,0 +1,36 @@
+#
+# It can be seen that the number, 125874, and its double, 251748,
+# contain exactly the same digits, but in a different order.
+#
+# Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+# contain the same digits.
+#
+
+
+def sameNums(a, b):
+    for i in str(b):
+        if(i in str(a)):
+            continue
+        else:
+            return False
+    return True
+
+
+def laskuri(n):
+    for i in range(2, 7):
+        if(sameNums(n, n*i)):
+            continue
+        else:
+            return False
+    return True
+
+
+def main():
+    num = 1
+    while(not laskuri(num)):
+        num += 1
+    print(num)
+
+
+if(__name__ == "__main__"):
+    main()
